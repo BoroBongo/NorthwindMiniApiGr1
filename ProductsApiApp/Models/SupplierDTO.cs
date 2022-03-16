15bcf1eff35db634f9bb8197a79366b8de1674d1
shakil-rahman <shax2020@gmail.com>
2022-03-16 19:39:40 +0000

    Created Category and Supplier DTO

---
+++ ProductsApiApp/Models/SupplierDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductsApiApp.Models
+{
+    public class SupplierDTO
+    {
+        public string CompanyName { get; set; } = null!;
+        public string? ContactName { get; set; }
+        public string? ContactTitle { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+        public string? Phone { get; set; }
+        public string? Fax { get; set; }
+        public string? HomePage { get; set; }
+    }
+}
