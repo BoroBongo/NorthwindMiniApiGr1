110614a7576b608d4a5f0caeb8a3d4bc7f8b9e85
BoroBongo <43158992+BoroBongo@users.noreply.github.com>
2022-03-16 14:27:16 +0000

    code doesn't smell anymore - deleted unused files

--- ProductsApiAppNew/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace ProductsApiAppNew.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
